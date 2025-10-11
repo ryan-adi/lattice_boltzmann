38498fde7e9364451dd8109571d19317931ad6cf
Ryan Adiputra <ryanadiputra230798@gmail.com>
2025-10-11 18:41:51 +0200

    move visualize functions to postprocessing

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,4 +1,4 @@
-from common_modules import plt, os, subprocess, glob, shutil
+from common_modules import plt, os, subprocess, glob, shutil, np
 
 def set_colorbar(im, label):
     cbar = plt.colorbar(im)
@@ -7,12 +7,44 @@ def set_colorbar(im, label):
 
 def create_output_folder(case_name):
     case_pngs = os.path.join("output", case_name, "pngs")
-    if not os.path.exists(case_pngs):
-        os.makedirs(case_pngs)
-    else:
+    case_csvs = os.path.join("output", case_name, "csvs")
+
+    if os.path.exists(case_pngs):
+        # if already exist delete case dir
         case_dir = os.path.join("output", case_name)
         shutil.rmtree(case_dir)
-        os.makedirs(case_pngs)
+        
+    os.makedirs(case_pngs)
+    os.makedirs(case_csvs)
+
+def save_csvs(case_name, current_iter, ds:dict):
+     
+    # define main directory
+    main_dir = os.getcwd()
+    case_csvs = os.path.join("output", case_name, "csvs")
+    
+    os.chdir(case_csvs)
+    # create csvs for all variables in ds
+    for var, data in ds.items():
+
+        var_name = ''.join([c for c in var if c.isupper()])
+        csv_name = f"t{current_iter:06d}_{var_name}.csv" 
+
+        if not os.path.exists(var_name):
+            os.mkdir(var_name)
+        os.chdir(var_name)
+        
+        # filter out zero entries
+        data[data < 1e-6] = 0
+
+        np.savetxt(csv_name, data, delimiter=",", fmt="%.3e")
+
+        os.chdir("..")
+
+
+
+    # go to directory where main is
+    os.chdir(main_dir)
 
 def save_pngs(case_name, current_iter, ds:dict):
     
@@ -30,13 +62,20 @@ def save_pngs(case_name, current_iter, ds:dict):
 
         var_name = ''.join([c for c in var if c.isupper()])
         png_name = f"t{current_iter:06d}_{var_name}.png" 
+
+        if not os.path.exists(var_name):
+            os.mkdir(var_name)
+        os.chdir(var_name)
+
         fig.savefig(png_name)
         plt.close()
+
+        os.chdir("..")
 
     # go to directory where main is
     os.chdir(main_dir)
 
-def generate_video(case_name:str, ds:dict, fps=20, save_pngs=True):
+def generate_video(case_name:str, ds:dict, fps=20, remove_pngs=True):
     # define pngs folder
     main_dir = os.getcwd()
     case_pngs = os.path.join("output", case_name, "pngs")
@@ -44,10 +83,14 @@ def generate_video(case_name:str, ds:dict, fps=20, save_pngs=True):
 
     # call ffmpeg to convert pngs to mp4
     for var, _ in ds.items():
-        # 
+        # file names
         var_name = ''.join([c for c in var if c.isupper()])
         video_name= case_name + "_" + var_name + ".mp4"
         png_name = "t%06d_" + var_name + ".png"
+
+        if not os.path.exists(var_name):
+            os.mkdir(var_name)
+        os.chdir(var_name)
 
         # call ffmpeg
         fps = str(fps)
@@ -57,11 +100,16 @@ def generate_video(case_name:str, ds:dict, fps=20, save_pngs=True):
             '-r', '30', 
             '-pix_fmt', 'yuv420p',
             video_name])
+        
+        # move mp4 to case dir
+        shutil.move(video_name, "../"+video_name)
 
         # remove pngs
-        if save_pngs:
+        if remove_pngs:
             for file_name in glob.glob("*_" + var_name + ".png"):
                 os.remove(file_name)
+
+        os.chdir("..")
 
     # go to directory where main is
     os.chdir(main_dir)
